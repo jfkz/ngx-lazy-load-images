97d9470f51ffb6dd8e9b6d5e570706a284175d93
mike <jfkz@users.noreply.github.com>
2022-08-01 22:21:41 +0000

    initial commit

---
+++ projects/ngx-lazy-load-images/src/lib/ngx-lazy-load-images.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { LazyLoadImagesDirective } from './ngx-lazy-load-images.directive';
+
+@NgModule({
+  declarations: [LazyLoadImagesDirective],
+  exports: [LazyLoadImagesDirective]
+})
+export class LazyLoadImagesModule {}
